3584b715fc4e8fe337533a74cb706910f292c026
Team <417skid@swerverobotics.club>
2025-11-06 20:32:33 -0800

    auto mirroring

--- team417/src/main/java/org/firstinspires/ftc/team417/CompetitionAuto.java
+++ team417/src/main/java/org/firstinspires/ftc/team417/CompetitionAuto.java
@@ -4,6 +4,7 @@ import com.acmerobotics.dashboard.canvas.Canvas;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
@@ -19,7 +20,7 @@ import org.firstinspires.ftc.team417.roadrunner.MecanumDrive;
  * This class exposes the competition version of Autonomous. As a general rule, add code to the
  * BaseOpMode class rather than here so that it can be shared between both TeleOp and Autonomous.
  */
-@Autonomous(name="Auto", group="Competition", preselectTeleOp="CompetitionTeleOp")
+@Autonomous(name = "Auto", group = "Competition", preselectTeleOp = "CompetitionTeleOp")
 public class CompetitionAuto extends BaseOpMode {
     enum Alliances {
         RED,
@@ -46,11 +47,14 @@ public class CompetitionAuto extends BaseOpMode {
 
         Pose2d blueFBNearStartPose = new Pose2d(-50, -50.5, Math.toRadians(139));
         Pose2d blueFBFarStartPose = new Pose2d(64, -16, Math.toRadians(180));
+        Pose2d SBNearStartPose = new Pose2d(-60, 48, Math.toRadians(139));
+        Pose2d SBFarStartPose = new Pose2d(64, 16, Math.toRadians(180));
+
 
         MecanumDrive drive = new MecanumDrive(hardwareMap, telemetry, gamepad1, startPose);
 
-            TextMenu menu = new TextMenu();
-            MenuInput menuInput = new MenuInput(MenuInput.InputType.CONTROLLER);
+        TextMenu menu = new TextMenu();
+        MenuInput menuInput = new MenuInput(MenuInput.InputType.CONTROLLER);
 
         // Text menu for FastBot
         if (MecanumDrive.isFastBot) {
@@ -66,7 +70,9 @@ public class CompetitionAuto extends BaseOpMode {
                     .add("wait-slider-1", new MenuSlider(minWaitTime, maxWaitTime))
                     .add()
                     .add("finish-button-1", new MenuFinishedButton());
-        // Text menu for SlowBot
+
+
+            // Text menu for SlowBot
         } else if (MecanumDrive.isSlowBot) {
             menu.add(new MenuHeader("AUTO SETUP"))
                     .add() // empty line for spacing
@@ -94,19 +100,26 @@ public class CompetitionAuto extends BaseOpMode {
             telemetry.update();
         }
 
+        Alliances chosenAlliance = menu.getResult(Alliances.class, "alliance-picker-1");
+        FastBotMovements chosenMovement = menu.getResult(FastBotMovements.class, "movement-picker-1");
+        double waitTime = menu.getResult(Double.class, "wait-slider-1");
+
+        PathFactory redPathFactory = new PathFactory(drive, false);
+
+        PathFactory bluePathFactory = new PathFactory(drive, true);
 
         // Red alliance FastBot auto paths
-        Action redNear = drive.actionBuilder(redFBNearStartPose)
+        Action redNearFastBot = drive.actionBuilder(redFBNearStartPose)
                 .setTangent(Math.toRadians(-49))
                 .stopAndAdd(new SpinUpAction())
                 .stopAndAdd(new LaunchAction())
                 .stopAndAdd(new LaunchAction())
                 .stopAndAdd(new LaunchAction())
 
-                .splineToLinearHeading(new Pose2d(-32,54,Math.toRadians(0)), Math.toRadians(90))
+                .splineToLinearHeading(new Pose2d(-32, 54, Math.toRadians(0)), Math.toRadians(90))
                 .build();
 
-        Action redFar = drive.actionBuilder(redFBFarStartPose)
+        Action redFarFastBot = drive.actionBuilder(redFBFarStartPose)
                 .setTangent(Math.toRadians(135))
                 .splineToLinearHeading(new Pose2d(-57, 36, Math.toRadians(0)), Math.toRadians(90))
                 .stopAndAdd(new SpinUpAction())
@@ -117,13 +130,13 @@ public class CompetitionAuto extends BaseOpMode {
                 .splineToLinearHeading(new Pose2d(-56, 12, Math.toRadians(0)), Math.toRadians(-90))
                 .build();
 
-        Action redFarMinimal = drive.actionBuilder(redFBFarStartPose)
-                .setTangent(Math.PI/2)
-                .splineTo(new Vector2d(56, 35), Math.PI/2)
+        Action redFarMinimalFastBot = drive.actionBuilder(redFBFarStartPose)
+                .setTangent(Math.PI / 2)
+                .splineTo(new Vector2d(56, 35), Math.PI / 2)
                 .build();
 
         // Blue alliance auto paths
-        Action blueNear = drive.actionBuilder(blueFBNearStartPose)
+        Action blueNearFastBot = drive.actionBuilder(blueFBNearStartPose)
                 .setTangent(Math.toRadians(49))
                 .stopAndAdd(new SpinUpAction())
                 .stopAndAdd(new LaunchAction())
@@ -131,10 +144,10 @@ public class CompetitionAuto extends BaseOpMode {
                 .stopAndAdd(new LaunchAction())
 //                .splineTo(new Vector2d(-44, -44), Math.toRadians(49))
 //                .setTangent(Math.toRadians(139))
-                .splineToLinearHeading(new Pose2d(-32,-54,Math.toRadians(180)), Math.toRadians(-90))
+                .splineToLinearHeading(new Pose2d(-32, -54, Math.toRadians(180)), Math.toRadians(-90))
                 .build();
 
-        Action blueFar = drive.actionBuilder(blueFBFarStartPose)
+        Action blueFarFastBot = drive.actionBuilder(blueFBFarStartPose)
                 .setTangent(Math.toRadians(-135))
                 .splineToLinearHeading(new Pose2d(-57, -36, Math.toRadians(180)), Math.toRadians(-90))
                 .stopAndAdd(new SpinUpAction())
@@ -145,32 +158,46 @@ public class CompetitionAuto extends BaseOpMode {
                 .splineToLinearHeading(new Pose2d(-56, -12, Math.toRadians(180)), Math.toRadians(90))
                 .build();
 
-        Action blueFarMinimal = drive.actionBuilder(blueFBFarStartPose)
-                .setTangent(Math.PI/2)
-                .splineTo(new Vector2d(56, -35), Math.PI/2)
+        Action blueFarMinimalFastBot = drive.actionBuilder(blueFBFarStartPose)
+                .setTangent(Math.PI / 2)
+                .splineTo(new Vector2d(56, -35), Math.PI / 2)
+                .build();
+
+        PathFactory pathFactory;
+
+        switch (chosenAlliance) {
+            case RED:
+                pathFactory = redPathFactory;
+                break;
+            case BLUE:
+                pathFactory = bluePathFactory;
+                break;
+            default:
+                throw new IllegalArgumentException("Alliance must be red or blue");
+        }
+
+        Action farMinimalSlowBot = pathFactory.actionBuilder(SBFarStartPose)
+                .setTangent(Math.toRadians(90))
+                .splineToLinearHeading(new Pose2d(48,32,Math.toRadians(180)), Math.toRadians(180))
                 .build();
 
         // the first parameter is the type to return as
         if (MecanumDrive.isFastBot) {
-            Alliances chosenAlliance = menu.getResult(Alliances.class, "alliance-picker-1");
-            FastBotMovements chosenMovement = menu.getResult(FastBotMovements.class, "movement-picker-1");
-            double waitTime = menu.getResult(Double.class, "wait-slider-1");
-
             Action trajectoryAction = null;
             switch (chosenAlliance) {
                 case RED:
                     switch (chosenMovement) {
                         case NEAR:
                             drive.setPose(redFBNearStartPose);
-                            trajectoryAction = redNear;
+                            trajectoryAction = redNearFastBot;
                             break;
                         case FAR:
                             drive.setPose(redFBFarStartPose);
-                            trajectoryAction = redFar;
+                            trajectoryAction = redFarFastBot;
                             break;
                         case FAR_MINIMAL:
                             drive.setPose(redFBFarStartPose);
-                            trajectoryAction = redFarMinimal;
+                            trajectoryAction = redFarMinimalFastBot;
                             break;
                     }
                     break;
@@ -179,15 +206,15 @@ public class CompetitionAuto extends BaseOpMode {
                     switch (chosenMovement) {
                         case NEAR:
                             drive.setPose(blueFBNearStartPose);
-                            trajectoryAction = blueNear;
+                            trajectoryAction = blueNearFastBot;
                             break;
                         case FAR:
                             drive.setPose(blueFBFarStartPose);
-                            trajectoryAction = blueFar;
+                            trajectoryAction = blueFarFastBot;
                             break;
                         case FAR_MINIMAL:
                             drive.setPose(blueFBFarStartPose);
-                            trajectoryAction = blueFarMinimal;
+                            trajectoryAction = blueFarMinimalFastBot;
                             break;
                     }
                     break;
@@ -224,46 +251,24 @@ public class CompetitionAuto extends BaseOpMode {
             }
 
         } else if (MecanumDrive.isSlowBot) {
-            Alliances chosenAlliance = menu.getResult(Alliances.class, "alliance-picker-1");
-            FastBotMovements chosenMovement = menu.getResult(FastBotMovements.class, "movement-picker-1");
-            double waitTime = menu.getResult(Double.class, "wait-slider-1");
-
             Action trajectoryAction = null;
-            switch (chosenAlliance) {
-                case RED:
-                    switch (chosenMovement) {
-                        case NEAR:
-                            drive.setPose(redFBNearStartPose);
-                            trajectoryAction = redNear;
-                            break;
-                        case FAR:
-                            drive.setPose(redFBFarStartPose);
-                            trajectoryAction = redFar;
-                            break;
-                        case FAR_MINIMAL:
-                            drive.setPose(redFBFarStartPose);
-                            trajectoryAction = redFarMinimal;
-                            break;
-                    }
-                    break;
 
-                case BLUE:
-                    switch (chosenMovement) {
-                        case NEAR:
-                            drive.setPose(blueFBNearStartPose);
-                            trajectoryAction = blueNear;
-                            break;
-                        case FAR:
-                            drive.setPose(blueFBFarStartPose);
-                            trajectoryAction = blueFar;
-                            break;
-                        case FAR_MINIMAL:
-                            drive.setPose(blueFBFarStartPose);
-                            trajectoryAction = blueFarMinimal;
-                            break;
-                    }
+            switch (chosenMovement) {
+                case NEAR:
+
+                    drive.setPose(SBNearStartPose);
+                    trajectoryAction = redNearFastBot;
+                    break;
+                case FAR:
+                    drive.setPose(SBFarStartPose);
+                    trajectoryAction = redFarFastBot;
+                    break;
+                case FAR_MINIMAL:
+                    drive.setPose(SBFarStartPose);
+                    trajectoryAction = farMinimalSlowBot;
                     break;
             }
+
 
             // Get a preview of the trajectory's path:
             Canvas previewCanvas = new Canvas();
@@ -295,5 +300,60 @@ public class CompetitionAuto extends BaseOpMode {
                 telemetry.update();
             }
         }
+    }
+}
+
+class PathFactory {
+    MecanumDrive drive;
+    TrajectoryActionBuilder builder;
+    boolean mirror;
+
+    public PathFactory(MecanumDrive drive, boolean mirror) {
+        this.drive = drive;
+        this.mirror = mirror;
+    }
+
+    Pose2d mirrorPose(Pose2d pose) {
+
+
+        return new Pose2d(pose.position.x, -pose.position.y, -pose.heading.log());
+    }
+
+    public PathFactory actionBuilder(Pose2d pose) {
+        if (mirror) {
+            builder = drive.actionBuilder(mirrorPose(pose));
+        } else {
+            builder = drive.actionBuilder(pose);
+        }
+        return this;
+    }
+
+    public PathFactory setTangent(double tangent) {
+        if (mirror) {
+            builder.setTangent(-tangent);
+        } else {
+            builder.setTangent(tangent);
+        }
+        return this;
+    }
+
+    public PathFactory splineToLinearHeading(Pose2d pose, double tangent) {
+
+        if (mirror) {
+            builder.splineToLinearHeading(mirrorPose(pose), -tangent);
+        } else {
+            builder.splineToLinearHeading(pose, tangent);
+        }
+        return this;
+    }
+
+
+    public Action build() {
+        return builder.build();
+    }
+
+    public PathFactory stopAndAdd(Action a) {
+        builder.stopAndAdd(a);
+        return this;
     }
 }
