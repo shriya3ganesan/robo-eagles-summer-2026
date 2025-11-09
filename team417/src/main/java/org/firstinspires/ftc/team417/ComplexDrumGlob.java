84768d315b5a4788735ebe6a4c6fb05bd89a92c0
Emmett <tigeremmett8@gmail.com>
2025-11-09 13:31:22 -0800

    comments

--- team417/src/main/java/org/firstinspires/ftc/team417/ComplexDrumGlob.java
+++ team417/src/main/java/org/firstinspires/ftc/team417/ComplexDrumGlob.java
@@ -4,34 +4,35 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.team417.roadrunner.MecanumDrive;
 
-enum LaunchColor {
+enum LaunchColor { //an enum for different color cases for launching
     PURPLE,
     GREEN,
     EITHER
 }
-class DrumGlob {
+class DrumGlob { //a placeholder class encompassing all code that ISN'T for slowbot.
     DrumGlob(){}
     void intake (double intakeValue){}
 
     void launch (LaunchColor launchColor) {}
 
-    static DrumGlob create (HardwareMap hardwareMap, Telemetry telemetry){
-        if (MecanumDrive.isSlowBot) {
-            return new ComplexDrumGlob(hardwareMap, telemetry);
 
-        } else {
-            return new DrumGlob();
+    static DrumGlob create (HardwareMap hardwareMap, Telemetry telemetry){
+        if (MecanumDrive.isSlowBot) { //if the robot is slowbot, use ComplexDrumGlob.
+            return new ComplexDrumGlob(hardwareMap, telemetry); //Go to ComplexDrumGlob class
+
+        } else { //otherwise, use DrumGlob
+            return new DrumGlob(); //Go to DrumGlob class
         }
     }
 }
 
-public class ComplexDrumGlob extends DrumGlob{
+public class ComplexDrumGlob extends DrumGlob{ //a class encompassing all code that IS for slowbot
 
     ComplexDrumGlob(HardwareMap hardwareMap, Telemetry telemetry){}
     @Override
-    void intake (double intakeValue){}
+    void intake (double intakeValue){} //a class that controls the intake based on intakeValue
 
-    void launch (LaunchColor launchColor) {}
+    void launch (LaunchColor launchColor) {} //a class that controls the launcher and transfer
 
 }
 
