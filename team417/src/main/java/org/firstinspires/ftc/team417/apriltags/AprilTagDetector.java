9570e292784d945e4ba351b23fe44e1cc72d04a0
Hankang Zhou <136044263+nobody-particular@users.noreply.github.com>
2025-11-09 16:15:33 -0800

    Made AprilTags detect artifact pattern in Autonomous! Tested on DevBot!

---
+++ team417/src/main/java/org/firstinspires/ftc/team417/apriltags/AprilTagDetector.java
@@ -0,0 +1,123 @@
+package org.firstinspires.ftc.team417.apriltags;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.team417.CompetitionAuto;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.io.Closeable;
+import java.util.Comparator;
+import java.util.List;
+
+public class AprilTagDetector implements Closeable {
+    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+
+    /**
+     * The variable to store our instance of the AprilTag processor.
+     */
+    private AprilTagProcessor aprilTag;
+
+    /**
+     * The variable to store our instance of the vision portal.
+     */
+    private VisionPortal visionPortal;
+
+    /**
+     * Initialize the AprilTag processor.
+     */
+    public void initAprilTag(HardwareMap hardwareMap) {
+        // Create the AprilTag processor the easy way.
+        aprilTag = AprilTagProcessor.easyCreateWithDefaults();
+
+        // Create the vision portal the easy way.
+        if (USE_WEBCAM) {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    hardwareMap.get(WebcamName.class, "camera"), aprilTag);
+        } else {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    BuiltinCameraDirection.BACK, aprilTag);
+        }
+
+    }   // end method initAprilTag()
+
+    /**
+     * Add telemetry about AprilTag detections.
+     */
+    public void telemetryAprilTag(Telemetry telemetry) {
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+        }   // end for() loop
+
+        // Add "key" information to telemetry
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        telemetry.addLine("RBE = Range, Bearing & Elevation");
+
+    }   // end method telemetryAprilTag()
+
+    public AprilTagDetection findDetection(CompetitionAuto.Alliance alliance) {
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+
+        // Remove all AprilTags that don't have ID 21, 22, or 23
+        currentDetections.removeIf(detection ->
+                detection.id != 21 && detection.id != 22 && detection.id != 23
+        );
+
+        switch (alliance) {
+            case RED:
+                // Return the leftmost detection relative to the robot
+                // If there are no detections, return null
+                return currentDetections.stream()
+                        .min(Comparator.comparingDouble(aprilTagDetection -> aprilTagDetection.ftcPose.x)).orElse(null);
+            case BLUE:
+                // Return the rightmost detection relative to the robot
+                // If there are no detections, return null
+                return currentDetections.stream()
+                        .max(Comparator.comparingDouble(aprilTagDetection -> aprilTagDetection.ftcPose.x)).orElse(null);
+        }
+
+        throw new IllegalArgumentException("Alliance must be red or blue");
+    }
+
+    public Pattern detectPattern(CompetitionAuto.Alliance alliance) {
+        AprilTagDetection detection = findDetection(alliance);
+
+        // Handle the case where no valid AprilTag was found
+        if (detection == null) {
+            return Pattern.UNKNOWN;
+        }
+
+        switch (detection.id) {
+            case 21:
+                return Pattern.GPP;
+            case 22:
+                return Pattern.PGP;
+            case 23:
+                return Pattern.PPG;
+        }
+
+        throw new IllegalArgumentException("ID must be 21, 22, or 23");
+    }
+
+    @Override
+    public void close() {
+        visionPortal.close();
+    }
+}
---
+++ team417/src/main/java/org/firstinspires/ftc/team417/apriltags/Pattern.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.team417.apriltags;
+
+public enum Pattern {
+    GPP,
+    PGP,
+    PPG,
+    UNKNOWN;
+}
