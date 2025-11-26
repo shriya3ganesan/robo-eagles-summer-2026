5b652f384d33b08638dd89a62ef6f58948bcd29b
crest0 <rancrest0@gmail.com>
2025-11-25 20:42:02 -0800

    changed inches to mm and html telemtry

--- team417/src/main/java/org/firstinspires/ftc/team417/TestColorDetect.java
+++ team417/src/main/java/org/firstinspires/ftc/team417/TestColorDetect.java
@@ -1,12 +1,16 @@
 package org.firstinspires.ftc.team417;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 @TeleOp
 public class TestColorDetect extends LinearOpMode {
     CoolColorDetector detector;
     @Override
     public void runOpMode() {
         detector = new CoolColorDetector(hardwareMap, telemetry);
+        telemetry.setDisplayFormat(Telemetry.DisplayFormat.HTML);
         waitForStart();
         while (opModeIsActive()) {
             detector.testTelemetry();
