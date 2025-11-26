061d9f87d05ee25b583db39ef1e4f8cc6da9a396
anya-codes <anya.rajyaguru@gmail.com>
2025-11-25 20:09:11 -0800

    Created new program to test launcher, feeder, and transfer without intake and drum for easier testing

---
+++ team417/src/main/java/org/firstinspires/ftc/team417/LauncherTest.java
@@ -0,0 +1,81 @@
+package org.firstinspires.ftc.team417;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+// Teleop without the intake and drum logic so we can just test for launcher speeds
+public class LauncherTest extends CompetitionTeleOp {
+    Servo servoTransfer;
+    DcMotorEx motLLauncher;
+    DcMotorEx motULauncher;
+    CRServo servoBLaunchFeeder;
+    CRServo servoFLaunchFeeder;
+    ElapsedTime transferTimer;
+    TransferState transferState;
+
+
+    enum TransferState {
+        WAIT,
+        DONE
+    }
+    public void initHardware() {
+        // Initializing only the transfer, feeder, and launcher motors/servos
+        servoTransfer = hardwareMap.get(Servo.class, "servoTransfer");
+        motLLauncher = hardwareMap.get(DcMotorEx.class, "motLLauncher");
+        motULauncher = hardwareMap.get(DcMotorEx.class, "motULauncher");
+        servoBLaunchFeeder = hardwareMap.get(CRServo.class, "servoBLaunchFeeder");
+        servoFLaunchFeeder = hardwareMap.get(CRServo.class, "servoFLaunchFeeder");
+
+        motLLauncher.setDirection(DcMotorSimple.Direction.REVERSE);
+        servoBLaunchFeeder.setDirection(CRServo.Direction.REVERSE);
+
+    }
+
+    public void runOpMode() {
+
+        // Spin up launcher flywheels to set flywheel velocities and start feeder wheels
+        if (gamepad2.dpadUpWasPressed()) {
+            motULauncher.setVelocity(ComplexMechGlob.UPPER_FAR_FLYWHEEL_VELOCITY);
+            motLLauncher.setVelocity(ComplexMechGlob.LOWER_FAR_FLYWHEEL_VELOCITY);
+            servoBLaunchFeeder.setPower(ComplexMechGlob.FEEDER_POWER);
+            servoFLaunchFeeder.setPower(ComplexMechGlob.FEEDER_POWER);
+
+        } else if (gamepad2.dpadDownWasPressed()) {
+            motULauncher.setVelocity(ComplexMechGlob.UPPER_NEAR_FLYWHEEL_VELOCITY);
+            motLLauncher.setVelocity(ComplexMechGlob.UPPER_NEAR_FLYWHEEL_VELOCITY);
+            servoBLaunchFeeder.setPower(ComplexMechGlob.FEEDER_POWER);
+            servoFLaunchFeeder.setPower(ComplexMechGlob.FEEDER_POWER);
+        }
+
+
+        // When y is pressed, start the transfer, run for TRANSFER_TIME_UP, then stop it
+        if (gamepad2.yWasPressed()) {
+            if (transferTimer == null) {
+                transferTimer = new ElapsedTime();
+            }
+            if (transferTimer.seconds() <= ComplexMechGlob.TRANSFER_TIME_UP) {
+                servoTransfer.setPosition(ComplexMechGlob.TRANSFER_ACTIVE_POSITION);
+                transferState = TransferState.WAIT;
+            }
+            if (transferTimer.seconds() >= ComplexMechGlob.TRANSFER_TIME_TOTAL) {
+                servoTransfer.setPosition(ComplexMechGlob.TRANSFER_INACTIVE_POSITION);
+                transferState = TransferState.DONE;
+                transferTimer = null;
+            }
+        }
+
+
+        // Stop all motors and feeders
+        if (gamepad2.dpadRightWasPressed()) {
+            servoBLaunchFeeder.setPower(0);
+            servoFLaunchFeeder.setPower(0);
+            motULauncher.setVelocity(0);
+            motLLauncher.setVelocity(0);
+        }
+
+    }
+
+}
