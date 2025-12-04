167a7a36522ded2ccf9238fe9187833595841178
anya-codes <anya.rajyaguru@gmail.com>
2025-12-03 17:35:08 -0800

    Changed robot wifi name from 417-b-RC to 417-B-RC

--- team417/src/main/java/org/firstinspires/ftc/team417/roadrunner/MecanumDrive.java
+++ team417/src/main/java/org/firstinspires/ftc/team417/roadrunner/MecanumDrive.java
@@ -207,7 +207,7 @@ public final class MecanumDrive {
     }
     public static boolean isDevBot = getBotName().equals("DevBot");
     public static boolean isFastBot = getBotName().equals("417-RC");
-    public static boolean isSlowBot = getBotName().equals("417-b-RC");
+    public static boolean isSlowBot = getBotName().equals("417-B-RC");
     public static Params PARAMS = new Params();
 
     public MecanumKinematics kinematics; // Initialized by initializeKinematics()
