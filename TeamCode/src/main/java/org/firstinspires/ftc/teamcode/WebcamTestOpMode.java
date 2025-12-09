c8d0f8db57c1551e058762daa01e63dcb3f4c764
Sturgeon II, Purser K <purerr.sturgeon@swri.org>
2025-12-09 09:41:04 -0600

    Added TAGID for blue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WebcamTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WebcamTestOpMode.java
@@ -29,7 +29,7 @@ public class WebcamTestOpMode  extends OpMode {
         aprilTagWebcam.update();
         AprilTagDetection id24 = aprilTagWebcam.getTagBySpecificId(24); // TAG ID 24 is the red goal
         aprilTagWebcam.displayDetectionTelemetry(id24);
-        // NOTE: we will need a separate OPMODE (otherwise identical) that sets the target TAGID to BLUE (#??)
+        // NOTE: we will need a separate OPMODE (otherwise identical) that sets the target TAGID to BLUE (#20)
         if(id24 != null) {
             double angleToTag = id24.ftcPose.yaw;
             turret.changeTurretByDegrees(angleToTag);
