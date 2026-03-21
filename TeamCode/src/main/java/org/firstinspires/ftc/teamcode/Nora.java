8ca08de5409a229190635c72cf0e489071e868d5
nrhnck <nora@tcrobotics.org>
2026-03-21 15:04:42 -0400

    nora+shayden=seamus

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Nora.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Nora.java
@@ -1,26 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "Nora TeleOp")
-public class Nora extends LinearOpMode {
+public class Nora extends OpMode {
     @Override
-    public void runOpMode() {
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
         int nora = 9;
         int shay = 12;
         int seamus = shay + nora;
 
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        // Wait for the driver to press PLAY
-        waitForStart();
-
-        // Run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-            telemetry.addData("Number", seamus);
-            telemetry.update();
-        }
+        telemetry.addData("Number", seamus);
     }
 }
+
