b8da79c78a787a023b515e7fd4dabcf349253220
Murali <muralirajeshkumar42@gmail.com>
2026-04-04 15:36:42 -0400

    mjdskdnfj

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop_V1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop_V1.java
@@ -1,4 +1,39 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-public class Teleop_V1 {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name="Teleop_V1")
+public class Teleop_V1 extends OpMode {
+    DcMotorEx frontLeftDrive;
+    DcMotorEx backLeftDrive;
+    DcMotorEx frontRightDrive;
+    DcMotorEx backRightDrive;
+
+    @Override
+    public void init() {
+        frontLeftDrive = hardwareMap.get(DcMotorEx.class, "frontLeftDrive");
+        backLeftDrive = hardwareMap.get(DcMotorEx.class, "backLeftDrive");
+        frontRightDrive = hardwareMap.get(DcMotorEx.class, "frontRightDrive");
+        backRightDrive = hardwareMap.get(DcMotorEx.class, "backRightDrive");
+        frontLeftDrive.setDirection(DcMotorEx.Direction.FORWARD);
+        backLeftDrive.setDirection(DcMotorEx.Direction.FORWARD);
+        frontRightDrive.setDirection(DcMotorEx.Direction.FORWARD);
+        backRightDrive.setDirection(DcMotorEx.Direction.FORWARD);
+    }
+
+    @Override
+    public void loop() {
+        double Power = gamepad1.right_stick_y * 0.25;
+
+        frontLeftDrive.setPower(Power);
+        backLeftDrive.setPower(Power);
+        frontRightDrive.setPower(Power);
+        backRightDrive.setPower(Power);
+
+        telemetry.addData("Current Power", Power);
+    }
 }
