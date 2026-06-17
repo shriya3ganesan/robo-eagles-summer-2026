0fc564c597e50442b32bc8a4f7a6dfa2fa0e1e86
anya-codes <anya.rajyaguru@gmail.com>
2026-06-16 20:48:37 -0700

    New branch for teleop with drum removed

--- team417/src/main/java/org/firstinspires/ftc/team417/BaseOpMode.java
+++ team417/src/main/java/org/firstinspires/ftc/team417/BaseOpMode.java
@@ -1,6 +1,12 @@
 package org.firstinspires.ftc.team417;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.team417.apriltags.LimelightAprilTagDetector;
 
@@ -14,6 +20,49 @@ abstract public class BaseOpMode extends LinearOpMode {
     public static double ROBOT_WIDTH = 16.15;
     public static double ROBOT_LENGTH = 16.5;
 
+    public static double MOTOR_D_VALUE = 1;
+
+    // TODO: tune constants in ftc dashboard
+    public static double FLYWHEEL_NEAR_SPEED = 800;
+    public static double FLYWHEEL_FAR_SPEED = 1000;
+    public static double FLYWHEEL_BACKSPIN = 300;
+    public static double WHEEL_STOP_SPEED = 0;
+    public static double TRANSFER_WHEEL_START_SPEED = 100;
+
+
+    DcMotorEx upperFlywheelMot;
+    DcMotorEx lowerFlywheelMot;
+    DcMotorEx transferWheelMot;
+    DcMotorEx intakeMot;
+
+    void initializeHardware() {
+        // Hardware map initialization
+        upperFlywheelMot = hardwareMap.get(DcMotorEx.class, "upperFlywheelMot");
+        lowerFlywheelMot = hardwareMap.get(DcMotorEx.class, "lowerFlywheelMot");
+        transferWheelMot = hardwareMap.get(DcMotorEx.class, "transferWheelMot");
+        intakeMot = hardwareMap.get(DcMotorEx.class, "intakeMot");
+
+        // Initializing motor behaviors
+        upperFlywheelMot.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        lowerFlywheelMot.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        intakeMot.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        transferWheelMot.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
+        upperFlywheelMot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lowerFlywheelMot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Set PID coefficients for flywheel
+        upperFlywheelMot.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(300, 0, MOTOR_D_VALUE, 10));
+        lowerFlywheelMot.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(300, 0, MOTOR_D_VALUE, 10));
+
+        // Set directions of motors
+        upperFlywheelMot.setDirection(DcMotor.Direction.REVERSE);
+        lowerFlywheelMot.setDirection(DcMotor.Direction.REVERSE);
+        transferWheelMot.setDirection(DcMotor.Direction.REVERSE);
+        intakeMot.setDirection(DcMotor.Direction.FORWARD);
+
+
+    }
 }
 
 
---
+++ team417/src/main/java/org/firstinspires/ftc/team417/CompetitionTeleopNew.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.team417;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+
+import org.firstinspires.ftc.team417.roadrunner.Drawing;
+import org.firstinspires.ftc.team417.roadrunner.MecanumDrive;
+
+
+/**
+ * This class exposes the competition version of TeleOp. As a general rule, add code to the
+ * BaseOpMode class rather than here so that it can be shared between both TeleOp and Autonomous.
+ */
+@TeleOp(name="TeleOp", group="Competition")
+public class CompetitionTeleopNew extends BaseOpMode {
+
+    @Override
+    public void runOpMode() {
+        Pose2d beginPose = new Pose2d(0, 0, 0);
+        MecanumDrive drive = new MecanumDrive(hardwareMap, telemetry, gamepad1, beginPose);
+
+        // Wait for Start to be pressed on the Driver Hub!
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addLine("Running TeleOp!");
+            telemetry.update();
+
+            // Set the drive motor powers according to the gamepad input:
+            drive.setDrivePowers(new PoseVelocity2d(
+                    new Vector2d(
+                            -gamepad1.left_stick_y,
+                            -gamepad1.left_stick_x
+                    ),
+                    -gamepad1.right_stick_x
+            ));
+
+
+            // Update the current pose:
+            drive.updatePoseEstimate();
+
+            // 'packet' is the object used to send data to FTC Dashboard:
+            TelemetryPacket packet = MecanumDrive.getTelemetryPacket();
+
+            // Do the work now for all active Road Runner actions, if any:
+            drive.doActionsWork(packet);
+
+            // Draw the robot and field:
+            packet.fieldOverlay().setStroke("#3F51B5");
+            Drawing.drawRobot(packet.fieldOverlay(), drive.pose);
+            MecanumDrive.sendTelemetryPacket(packet);
+
+            // Controls Below
+            if
+
+
+            if (gamepad2.dpad up) {
+
+            }
+
+        }
+    }
+}
