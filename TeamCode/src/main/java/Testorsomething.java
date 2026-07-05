2d35d3e3527b0c820ef5b370febcb4987de28546
snoopytreehouse-lgtm <snoopytreehouse@gmail.com>
2026-07-05 13:38:39 -0700

    Create Testorsomething.java

---
+++ TeamCode/src/main/java/Testorsomething.java
@@ -0,0 +1,8 @@
+public class Testorsomething {
+    //This is a test or something
+    //ok, cool
+    //and now
+    //I guess we'll try pushing it.
+    //Ok, I guess we're just typing everything I'm saying.
+    
+}
