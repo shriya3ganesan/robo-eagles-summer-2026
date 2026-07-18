85ed408f292cf0478602d94452365e8bf8c24133
shriya3ganesan <shriya3ganesan@gmail.com>
2026-07-18 11:45:39 -0500

    Added TeleOp Template for 7-18 Lesson

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exercises/ArjunB/TeleOp_Template.java
@@ -0,0 +1,57 @@
+
+package org.firstinspires.ftc.teamcode.Exercises.ArjunB;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@TeleOp(name="Basic: Iterative OpMode", group="Iterative OpMode")
+@Disabled
+public class TeleOp_Template extends OpMode
+{
+    private ElapsedTime runtime = new ElapsedTime();
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit START
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits START
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits START but before they hit STOP
+     */
+    @Override
+    public void loop() {
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+}
