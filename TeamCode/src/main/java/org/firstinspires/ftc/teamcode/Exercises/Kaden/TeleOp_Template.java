e3b44d3ac62f8a58b3ca064c837fc5c3a5a35d38
KadenB4 <kadenbbb5@gmail.com>
2026-07-18 14:55:14 -0500

    added hardware map

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exercises/Kaden/TeleOp_Template.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exercises/Kaden/TeleOp_Template.java
@@ -4,21 +4,33 @@ package org.firstinspires.ftc.teamcode.Exercises.Kaden;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 @TeleOp(name="Basic: Iterative OpMode", group="Iterative OpMode")
 @Disabled
-public class TeleOp_Template extends OpMode
-{
+public class TeleOp_Template extends OpMode {
     private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor fl = null;
+    private DcMotor fr = null;
+    private DcMotor bl = null;
+    private DcMotor br = null;
 
     /*
      * Code to run ONCE when the driver hits INIT
      */
     @Override
     public void init() {
+        fl = hardwareMap.get (DcMotor.class, "fl");
+        fr = hardwareMap.get (DcMotor.class, "fr");
+        bl = hardwareMap.get (DcMotor.class, "bl");
+        br = hardwareMap.get (DcMotor.class, "br");
         telemetry.addData("Status", "Initialized");
+
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
         // Tell the driver that initialization is complete.
@@ -50,8 +62,10 @@ public class TeleOp_Template extends OpMode
     /*
      * Code to run ONCE after the driver hits STOP
      */
+
     @Override
     public void stop() {
     }
 
 }
+
