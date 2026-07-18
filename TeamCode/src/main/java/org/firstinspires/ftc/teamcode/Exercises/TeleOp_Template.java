7f36fc6bba3a7e58a5cb3d0a58f3147a81190085
nithila-pp <pp.nithila@gmail.com>
2026-07-18 14:57:28 -0500

    I edited the TeleOp template

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exercises/TeleOp_Template.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exercises/TeleOp_Template.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -14,6 +15,11 @@ import com.qualcomm.robotcore.util.Range;
 public class TeleOp_Template extends OpMode
 {
     private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor fl = null;
+    private DcMotor fr = null;
+    private DcMotor bl = null;
+    private DcMotor br = null;
+
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -21,6 +27,14 @@ public class TeleOp_Template extends OpMode
     @Override
     public void init() {
         telemetry.addData("Status", "Initialized");
+
+        fl = hardwareMap.get(DcMotor.class, "Fl");
+        fr = hardwareMap.get(DcMotor.class, "Fr");
+        bl = hardwareMap.get(DcMotor.class, "br");
+        br = hardwareMap.get(DcMotor.class, "bl");
+
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
         // Tell the driver that initialization is complete.
