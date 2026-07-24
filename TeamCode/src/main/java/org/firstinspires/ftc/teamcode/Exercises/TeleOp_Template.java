7d3ec5a29ff0fe8ab8efeb8c4516c9b2bb42eebe
AniGupta-1 <archanagupta731@gmail.com>
2026-07-24 15:40:57 +0530

    newly updated code 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exercises/TeleOp_Template.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exercises/TeleOp_Template.java
@@ -12,9 +12,7 @@ import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Basic: Iterative OpMode", group="Iterative OpMode")
 @Disabled
-public class TeleOp_Template extends OpMode
-{
-
+public class TeleOp_Template extends OpMode {
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor fl = null;
     private DcMotor fr = null;
@@ -26,12 +24,11 @@ public class TeleOp_Template extends OpMode
      */
     @Override
     public void init() {
-        telemetry.addData("Status", "Initialized");
-
         fl = hardwareMap.get(DcMotor.class, "fl");
         fr = hardwareMap.get(DcMotor.class, "fr");
         bl = hardwareMap.get(DcMotor.class, "bl");
         br = hardwareMap.get(DcMotor.class, "br");
+        telemetry.addData("Status", "Initialized");
 
         fr.setDirection(DcMotorSimple.Direction.REVERSE);
         br.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -61,13 +58,60 @@ public class TeleOp_Template extends OpMode
      */
     @Override
     public void loop() {
+
+
+        /*
+         * Code to run ONCE after the driver hits STOP
+         */
+
+        //Left joystick goes forward and backward
+//        if (Math.abs(gamepad1.left_stick_y) > .05) {
+//            fl.setPower(gamepad1.left_stick_y);
+//            fr.setPower(gamepad1.left_stick_y);
+//            bl.setPower(gamepad1.left_stick_y);
+//            br.setPower(gamepad1.left_stick_y);
+//        } else {
+//        fl.setPower(0);
+//        fr.setPower(0);
+//        bl.setPower(0);
+//        br.setPower(0);
+//    }
+//
+//        //right joystick turns
+//
+//        if (Math.abs(gamepad1.right_stick_x) > .05) {
+//            fl.setPower(gamepad1.right_stick_x);
+//            fr.setPower(-gamepad1.right_stick_x);
+//            bl.setPower(gamepad1.right_stick_x);
+//            br.setPower(-gamepad1.right_stick_x);
+//        } else {
+//
+//        fl.setPower(0);
+//        fr.setPower(0);
+//        bl.setPower(0);
+//        br.setPower(0);
+
+
+        double left_power;
+        double right_power;
+
+        left_power = Range.clip(gamepad1.left_stick_y + gamepad1.right_stick_x, -1, 1);
+        right_power = Range.clip(gamepad1.left_stick_y - gamepad1.right_stick_x, -1, 1);
+
+        if (Math.abs(left_power) < .05) {
+            left_power = 0;
+        }
+        if (Math.abs(right_power) > .05) {
+            right_power = 0;
+        }
+
+        fl.setPower(left_power);
+        fr.setPower(right_power);
+        bl.setPower(left_power);
+        br.setPower(right_power);
     }
 
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-    @Override
-    public void stop() {
-    }
-
+//        @Override
+//        public void stop() {}
 }
+
